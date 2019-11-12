96b08ad393f3009dc73d4886a63d37a3889359fc
gmorae <gmmartins06@gmail.com>
2019-11-12 13:24:06 -0200

    Colocando o footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import './App.css'
 function App() {
   return (
     <div className="App">
-      
+      <h1>Home</h1>
+      <footer>Escola Senai de informática</footer>
     </div>
   );
 }

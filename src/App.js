e17a03c3a077b5995a3deb6262a155bfd22cd2df
gmorae <gmmartins06@gmail.com>
2019-11-12 14:42:28 -0200

    Criando o component navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css'
 import Rodape from './components/Rodape'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Home</h1>
       <Rodape />
     </div>
--- src/pages/Categoria.js
+++ src/pages/Categoria.js
@@ -1,10 +1,13 @@
 import React, { Component} from 'react'
 import '../App.css'
 import Rodape from '../components/Rodape'
+import Navbar from '../components/Navbar'
+
 class Categoria extends Component{
     render(){
         return(
             <div className="App">
+                <Navbar />
                 <h1>Categoria</h1>
                 <p>Teste Categoria</p>
                 <Rodape />

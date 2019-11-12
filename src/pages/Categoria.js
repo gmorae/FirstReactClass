bf567ac594b415a2197e0df1d1b9ff15534d2b01
gmorae <gmmartins06@gmail.com>
2019-11-12 13:34:34 -0200

    Criando a página de categoria

---
+++ src/pages/Categoria.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react'
+import '../App.css'
+class Categoria extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Categoria</h1>
+                <p>Teste Categiria</p>
+                <footer>Escola Senai de informática</footer>
+            </div>
+        )
+    }
+}
+
+export default Categoria
